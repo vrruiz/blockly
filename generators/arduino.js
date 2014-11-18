0196bab9d101a1a5b583a4e9754a6c452707cbb8
Sergio Morcuende <sergio.morcuende@bq.com>
2014-11-18 17:50:08 +0100

    LOGIC_OPERATION AND char fixed

--- generators/arduino.js
+++ generators/arduino.js
@@ -22,7 +22,7 @@ Blockly.Msg.EXPAND_ALL = "Expand Blocks";
 
 
 
-Blockly.Arduino=new Blockly.Generator("Arduino");
+Blockly.Arduino = new Blockly.Generator("Arduino");
 /**
  * List of illegal variable names.
  * This is not intended to be a security feature.  Blockly is 100% client-side,
@@ -31,53 +31,79 @@ Blockly.Arduino=new Blockly.Generator("Arduino");
  * @private
  */
 if (!Blockly.Arduino.RESERVED_WORDS_) {
-  Blockly.Arduino.RESERVED_WORDS_ = '';
+    Blockly.Arduino.RESERVED_WORDS_ = '';
 }
 
 Blockly.Arduino.RESERVED_WORDS_ +=
-    // http://arduino.cc/en/Reference/HomePage
-'setup,loop,if,else,for,switch,case,while,do,break,continue,return,goto,define,include,HIGH,LOW,INPUT,OUTPUT,INPUT_PULLUP,true,false,interger, constants,floating,point,void,bookean,char,unsigned,byte,int,word,long,float,double,string,String,array,static, volatile,const,sizeof,pinMode,digitalWrite,digitalRead,analogReference,analogRead,analogWrite,tone,noTone,shiftOut,shitIn,pulseIn,millis,micros,delay,delayMicroseconds,min,max,abs,constrain,map,pow,sqrt,sin,cos,tan,randomSeed,random,lowByte,highByte,bitRead,bitWrite,bitSet,bitClear,bit,attachInterrupt,detachInterrupt,interrupts,noInterrupts'
-;
+// http://arduino.cc/en/Reference/HomePage
+'setup,loop,if,else,for,switch,case,while,do,break,continue,return,goto,define,include,HIGH,LOW,INPUT,OUTPUT,INPUT_PULLUP,true,false,interger, constants,floating,point,void,bookean,char,unsigned,byte,int,word,long,float,double,string,String,array,static, volatile,const,sizeof,pinMode,digitalWrite,digitalRead,analogReference,analogRead,analogWrite,tone,noTone,shiftOut,shitIn,pulseIn,millis,micros,delay,delayMicroseconds,min,max,abs,constrain,map,pow,sqrt,sin,cos,tan,randomSeed,random,lowByte,highByte,bitRead,bitWrite,bitSet,bitClear,bit,attachInterrupt,detachInterrupt,interrupts,noInterrupts';
 
 /**
  * Order of operation ENUMs.
- * 
+ *
  */
-Blockly.Arduino.ORDER_ATOMIC = 0;         // 0 "" ...
-Blockly.Arduino.ORDER_UNARY_POSTFIX = 1;  // expr++ expr-- () [] .
-Blockly.Arduino.ORDER_UNARY_PREFIX = 2;   // -expr !expr ~expr ++expr --expr
+Blockly.Arduino.ORDER_ATOMIC = 0; // 0 "" ...
+Blockly.Arduino.ORDER_UNARY_POSTFIX = 1; // expr++ expr-- () [] .
+Blockly.Arduino.ORDER_UNARY_PREFIX = 2; // -expr !expr ~expr ++expr --expr
 Blockly.Arduino.ORDER_MULTIPLICATIVE = 3; // * / % ~/
-Blockly.Arduino.ORDER_ADDITIVE = 4;       // + -
-Blockly.Arduino.ORDER_SHIFT = 5;          // << >>
-Blockly.Arduino.ORDER_RELATIONAL = 6;     // is is! >= > <= <
-Blockly.Arduino.ORDER_EQUALITY = 7;       // == != === !==
-Blockly.Arduino.ORDER_BITWISE_AND = 8;    // &
-Blockly.Arduino.ORDER_BITWISE_XOR = 9;    // ^
-Blockly.Arduino.ORDER_BITWISE_OR = 10;    // |
-Blockly.Arduino.ORDER_LOGICAL_AND = 11;   // &&
-Blockly.Arduino.ORDER_LOGICAL_OR = 12;    // ||
-Blockly.Arduino.ORDER_CONDITIONAL = 13;   // expr ? expr : expr
-Blockly.Arduino.ORDER_ASSIGNMENT = 14;    // = *= /= ~/= %= += -= <<= >>= &= ^= |=
-Blockly.Arduino.ORDER_NONE = 99;          // (...)
+Blockly.Arduino.ORDER_ADDITIVE = 4; // + -
+Blockly.Arduino.ORDER_SHIFT = 5; // << >>
+Blockly.Arduino.ORDER_RELATIONAL = 6; // is is! >= > <= <
+Blockly.Arduino.ORDER_EQUALITY = 7; // == != === !==
+Blockly.Arduino.ORDER_BITWISE_AND = 8; // &
+Blockly.Arduino.ORDER_BITWISE_XOR = 9; // ^
+Blockly.Arduino.ORDER_BITWISE_OR = 10; // |
+Blockly.Arduino.ORDER_LOGICAL_AND = 11; // &&
+Blockly.Arduino.ORDER_LOGICAL_OR = 12; // ||
+Blockly.Arduino.ORDER_CONDITIONAL = 13; // expr ? expr : expr
+Blockly.Arduino.ORDER_ASSIGNMENT = 14; // = *= /= ~/= %= += -= <<= >>= &= ^= |=
+Blockly.Arduino.ORDER_NONE = 99; // (...)
 
 /*
  * Arduino Board profiles
  *
  */
 var profile = {
-  arduino: {
-    description: "Arduino standard-compatible board",
-    digital : [["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"], ["6", "6"], ["7", "7"], ["8", "8"], ["9", "9"], ["10", "10"], ["11", "11"], ["12", "12"], ["13", "13"], ["A0", "A0"], ["A1", "A1"], ["A2", "A2"], ["A3", "A3"], ["A4", "A4"], ["A5", "A5"]],
-    analog : [["A0", "A0"], ["A1", "A1"], ["A2", "A2"], ["A3", "A3"], ["A4", "A4"], ["A5", "A5"]],
-        serial : 9600
-  },
-  arduino_mega:{
-    description: "Arduino Mega-compatible board"
-    //53 digital
-    //15 analog
-  }
-}
-//set default profile to arduino standard-compatible board
+        arduino: {
+            description: "Arduino standard-compatible board",
+            digital: [
+                ["1", "1"],
+                ["2", "2"],
+                ["3", "3"],
+                ["4", "4"],
+                ["5", "5"],
+                ["6", "6"],
+                ["7", "7"],
+                ["8", "8"],
+                ["9", "9"],
+                ["10", "10"],
+                ["11", "11"],
+                ["12", "12"],
+                ["13", "13"],
+                ["A0", "A0"],
+                ["A1", "A1"],
+                ["A2", "A2"],
+                ["A3", "A3"],
+                ["A4", "A4"],
+                ["A5", "A5"]
+            ],
+            analog: [
+                ["A0", "A0"],
+                ["A1", "A1"],
+                ["A2", "A2"],
+                ["A3", "A3"],
+                ["A4", "A4"],
+                ["A5", "A5"]
+            ],
+            serial: 9600
+        },
+        arduino_mega: {
+            description: "Arduino Mega-compatible board"
+            //53 digital
+            //15 analog
+        }
+    }
+    //set default profile to arduino standard-compatible board
 profile["default"] = profile["arduino"];
 
 
@@ -85,19 +111,19 @@ profile["default"] = profile["arduino"];
  * Initialise the database of variable names.
  */
 Blockly.Arduino.init = function() {
-  // Create a dictionary of definitions to be printed before setups.
-  Blockly.Arduino.definitions_ = {};
-  // Create a dictionary of setups to be printed before the code.
-  Blockly.Arduino.setups_ = {};
-  
-  if (Blockly.Variables) {
-    if (!Blockly.Arduino.variableDB_) {
-      Blockly.Arduino.variableDB_ =
-          new Blockly.Names(Blockly.Arduino.RESERVED_WORDS_);
-    } else {
-      Blockly.Arduino.variableDB_.reset();
+    // Create a dictionary of definitions to be printed before setups.
+    Blockly.Arduino.definitions_ = {};
+    // Create a dictionary of setups to be printed before the code.
+    Blockly.Arduino.setups_ = {};
+
+    if (Blockly.Variables) {
+        if (!Blockly.Arduino.variableDB_) {
+            Blockly.Arduino.variableDB_ =
+                new Blockly.Names(Blockly.Arduino.RESERVED_WORDS_);
+        } else {
+            Blockly.Arduino.variableDB_.reset();
+        }
     }
-  }
 };
 
 
@@ -107,45 +133,45 @@ Blockly.Arduino.init = function() {
  * @return {string} Completed code.
  */
 Blockly.Arduino.finish = function(code) {
-  // Indent every line.
-  code = '  ' + code.replace(/\n/g, '\n  ');
-  code = code.replace(/\n\s+$/, '\n');
-  code = 'void loop() \n{\n' + code + '\n}';
+    // Indent every line.
+    code = '  ' + code.replace(/\n/g, '\n  ');
+    code = code.replace(/\n\s+$/, '\n');
+    code = 'void loop() \n{\n' + code + '\n}';
 
-  // Convert the definitions dictionary into a list.
-  var imports = [];
-  var definitions = [];
-  for (var name in Blockly.Arduino.definitions_) {
-    var def = Blockly.Arduino.definitions_[name];
-    if (def.match(/^#include/)) {
-      imports.push(def);
-    } else {
-      definitions.push(def);
+    // Convert the definitions dictionary into a list.
+    var imports = [];
+    var definitions = [];
+    for (var name in Blockly.Arduino.definitions_) {
+        var def = Blockly.Arduino.definitions_[name];
+        if (def.match(/^#include/)) {
+            imports.push(def);
+        } else {
+            definitions.push(def);
+        }
     }
-  }
-  
-  // Convert the setups dictionary into a list.
-  var setups = [];
-  for (var name in Blockly.Arduino.setups_) {
-    setups.push(Blockly.Arduino.setups_[name]);
-  }
-  
-  var allDefs = imports.join('\n') + '\n\n' + definitions.join('\n') + '\nvoid setup() \n{\n  '+setups.join('\n  ') + '\n}'+ '\n\n';
+
+    // Convert the setups dictionary into a list.
+    var setups = [];
+    for (var name in Blockly.Arduino.setups_) {
+        setups.push(Blockly.Arduino.setups_[name]);
+    }
+
+    var allDefs = imports.join('\n') + '\n\n' + definitions.join('\n') + '\nvoid setup() \n{\n  ' + setups.join('\n  ') + '\n}' + '\n\n';
 
 
-  var allCode=allDefs.replace(/\n\n+/g, '\n\n').replace(/\n*$/, '\n\n\n') + code;
+    var allCode = allDefs.replace(/\n\n+/g, '\n\n').replace(/\n*$/, '\n\n\n') + code;
 
-  allCode=allCode.replace(/&quot;/g,'"');
-  allCode=allCode.replace(/quot;/g,'"');
-  allCode=allCode.replace(/&amp;/g,'');
-  allCode=allCode.replace(/amp;/g,'');
+    allCode = allCode.replace(/&quot;/g, '"');
+    allCode = allCode.replace(/quot;/g, '"');
+    allCode = allCode.replace(/&amp;/g, '&');
+    allCode = allCode.replace(/amp;/g, '&');
 
-  allCode=allCode.replace(/&lt;/g,'<');
-  allCode=allCode.replace(/lt;/g,'<');
-  allCode=allCode.replace(/&gt;/g,'>');
-  allCode=allCode.replace(/gt;/g,'>');
+    allCode = allCode.replace(/&lt;/g, '<');
+    allCode = allCode.replace(/lt;/g, '<');
+    allCode = allCode.replace(/&gt;/g, '>');
+    allCode = allCode.replace(/gt;/g, '>');
 
-  return allCode;
+    return allCode;
 };
 
 /**
@@ -155,7 +181,7 @@ Blockly.Arduino.finish = function(code) {
  * @return {string} Legal line of code.
  */
 Blockly.Arduino.scrubNakedValue = function(line) {
-  return line + ';\n';
+    return line + ';\n';
 };
 
 /**
@@ -165,12 +191,12 @@ Blockly.Arduino.scrubNakedValue = function(line) {
  * @private
  */
 Blockly.Arduino.quote_ = function(string) {
-  // TODO: This is a quick hack.  Replace with goog.string.quote
-  string = string.replace(/\\/g, '\\\\')
-                 .replace(/\n/g, '\\\n')
-                 .replace(/\$/g, '\\$')
-                 .replace(/'/g, '\\\'');
-  return '\"' + string + '\"';
+    // TODO: This is a quick hack.  Replace with goog.string.quote
+    string = string.replace(/\\/g, '\\\\')
+        .replace(/\n/g, '\\\n')
+        .replace(/\$/g, '\\$')
+        .replace(/'/g, '\\\'');
+    return '\"' + string + '\"';
 };
 
 /**
@@ -184,33 +210,33 @@ Blockly.Arduino.quote_ = function(string) {
  * @private
  */
 Blockly.Arduino.scrub_ = function(block, code) {
-  if (code === null) {
-    // Block has handled code generation itself.
-    return '';
-  }
-  var commentCode = '';
-  // Only collect comments for blocks that aren't inline.
-  if (!block.outputConnection || !block.outputConnection.targetConnection) {
-    // Collect comment for this block.
-    var comment = block.getCommentText();
-    if (comment) {
-      commentCode += Blockly.Generator.prefixLines(comment, '// ') + '\n';
+    if (code === null) {
+        // Block has handled code generation itself.
+        return '';
     }
-    // Collect comments for all value arguments.
-    // Don't collect comments for nested statements.
-    for (var x = 0; x < block.inputList.length; x++) {
-      if (block.inputList[x].type == Blockly.INPUT_VALUE) {
-        var childBlock = block.inputList[x].connection.targetBlock();
-        if (childBlock) {
-//          var comment = Blockly.Generator.allNestedComments(childBlock);
-          if (comment) {
-            commentCode += Blockly.Generator.prefixLines(comment, '// ');
-          }
+    var commentCode = '';
+    // Only collect comments for blocks that aren't inline.
+    if (!block.outputConnection || !block.outputConnection.targetConnection) {
+        // Collect comment for this block.
+        var comment = block.getCommentText();
+        if (comment) {
+            commentCode += Blockly.Generator.prefixLines(comment, '// ') + '\n';
         }
-      }
+        // Collect comments for all value arguments.
+        // Don't collect comments for nested statements.
+        for (var x = 0; x < block.inputList.length; x++) {
+            if (block.inputList[x].type == Blockly.INPUT_VALUE) {
+                var childBlock = block.inputList[x].connection.targetBlock();
+                if (childBlock) {
+                    //          var comment = Blockly.Generator.allNestedComments(childBlock);
+                    if (comment) {
+                        commentCode += Blockly.Generator.prefixLines(comment, '// ');
+                    }
+                }
+            }
+        }
     }
-  }
-  var nextBlock = block.nextConnection && block.nextConnection.targetBlock();
-  var nextCode = this.blockToCode(nextBlock);
-  return commentCode + code + nextCode;
+    var nextBlock = block.nextConnection && block.nextConnection.targetBlock();
+    var nextCode = this.blockToCode(nextBlock);
+    return commentCode + code + nextCode;
 };
